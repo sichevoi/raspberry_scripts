5eaaba2675633db9604cb1961c2b741ba1e62a02
Elvira Sichevaia <esichevaia.test@gmail.com>
2019-09-26 02:40:42 +0100

    Updated wget-download script

--- cont_download.sh
+++ cont_download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while [ 1 ] 
 do
-	timeout 60 wget http://ipv4.download.thinkbroadband.com/512MB.zip -O tmp.tmp
+	wget -T 60 -t 3 http://ipv4.download.thinkbroadband.com/512MB.zip -O tmp.tmp
         sleep 40
 done
