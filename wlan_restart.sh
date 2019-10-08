df8ae1691287923e06ad379e255299825ed20be2
Elvira Sichevaia <esichevaia.test@gmail.com>
2019-10-08 13:58:27 +0100

    Updated wlan_restart script for more sessions

--- wlan_restart.sh
+++ wlan_restart.sh
@@ -3,8 +3,11 @@ set -x
 while [ 1 ] 
 do
     sudo ifconfig wlan0 down
-    sleep 30
+    sleep 20
     sudo ifconfig wlan0 up
-    sleep 30
+    sleep 20
+    ping -c 10 8.8.8.8
+    wget -T 60 -t 3 http://ipv4.download.thinkbroadband.com/512MB.zip -O tmp.tmp
+    ping -c 10 216.58.211.110
 done
 
